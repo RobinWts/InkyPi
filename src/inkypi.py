7d49cb3850e175a5dc57177d9cb9fdc26ae9ee25
RobinWts <wortspiele.tapioka_0i@icloud.com>
2026-02-06 11:12:11 +0100

    minimum core changes, generic blueprint reg for plugins

--- src/inkypi.py
+++ src/inkypi.py
@@ -31,7 +31,7 @@ from blueprints.plugin import plugin_bp
 from blueprints.playlist import playlist_bp
 from blueprints.apikeys import apikeys_bp
 from jinja2 import ChoiceLoader, FileSystemLoader
-from plugins.plugin_registry import load_plugins, get_plugin_instance
+from plugins.plugin_registry import load_plugins, get_plugin_instance, register_plugin_blueprints
 from waitress import serve
 
 
@@ -81,20 +81,8 @@ app.register_blueprint(plugin_bp)
 app.register_blueprint(playlist_bp)
 app.register_blueprint(apikeys_bp)
 
-# Register plugin blueprints (plugins can expose blueprints via get_blueprint() static method)
-try:
-    pluginmanager_config = device_config.get_plugin("pluginmanager")
-    if pluginmanager_config:
-        # Access the plugin class to call the static method
-        from plugins.plugin_registry import PLUGIN_CLASSES
-        pluginmanager_class = PLUGIN_CLASSES.get("pluginmanager")
-        if pluginmanager_class and hasattr(pluginmanager_class, 'get_blueprint'):
-            bp = pluginmanager_class.get_blueprint()
-            if bp:
-                app.register_blueprint(bp)
-                logger.info("Registered blueprint for pluginmanager plugin")
-except Exception as e:
-    logger.debug(f"Could not register pluginmanager blueprint: {e}")
+# Register blueprints from plugins (generic mechanism - any plugin can expose blueprints)
+register_plugin_blueprints(app)
 
 # Register opener for HEIF/HEIC images
 register_heif_opener()
